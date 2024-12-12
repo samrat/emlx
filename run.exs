5de25ddfa6cd6366ae295bf897f4cb2986058ce9
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-12-12 05:51:03 -0300

    chore: remove bench files

--- run.exs
+++
@@ -1,7 +0,0 @@
-System.pid() |> dbg()
-IO.gets("Press enter to run...")
-
-Nx.global_default_backend(EMLX.Backend)
-
-Nx.Defn.jit_apply(&{Nx.add(&1, &2), Nx.subtract(&1, &2)}, [Nx.tensor(1), Nx.tensor(2)], compiler: EMLX)
-|> dbg()
