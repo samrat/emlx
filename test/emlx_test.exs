39f5256288a94e415510c7735ffe9e4fcb1fce4b
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-12-12 06:01:21 -0300

    chore: forat

--- test/emlx_test.exs
+++ test/emlx_test.exs
@@ -4,9 +4,7 @@ defmodule EMLXTest do
 
   test "__jit__" do
     {left, right} =
-      Nx.Defn.jit_apply(&{Nx.add(&1, &2), Nx.subtract(&1, &2)}, [Nx.tensor(1), 2],
-        compiler: EMLX
-      )
+      Nx.Defn.jit_apply(&{Nx.add(&1, &2), Nx.subtract(&1, &2)}, [Nx.tensor(1), 2], compiler: EMLX)
 
     assert_equal(left, Nx.tensor(3))
     assert_equal(right, Nx.tensor(-1))
